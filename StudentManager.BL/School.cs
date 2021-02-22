2847ec2e109a89e24fa8c02c7984760f2a75c327
Orlando <lopezorlando23@gmail.com>
2021-02-22 13:50:33 -0700

    Added additional Data
    Added some additional data in the school class

--- StudentManager.BL/School.cs
+++ StudentManager.BL/School.cs
@@ -18,11 +18,14 @@ namespace StudentManager.BL
             // Current Programs
             Program program1 = new Program() { id = 1, name = "Quantum Physics" };
             Program program2 = new Program() { id = 2, name = "C# - Intro" };
+            Program program3 = new Program() { id = 3, name = "SQL and Databases" };
+            Program program4 = new Program() { id = 4, name = ".NET and MVC" };
 
             // add programs to the Programs List
             Programs.Add(program1);
             Programs.Add(program2);
-
+            Programs.Add(program3);
+            Programs.Add(program4);
 
             // List for Students
             Students = new List<Student>();
@@ -30,8 +33,19 @@ namespace StudentManager.BL
             // Current Students
             Student student1 = new Student() { ID = 1, Name = "Todd Bummer", Program=program1};
             Student student2 = new Student() { ID = 2, Name = "John James", Program = program2 };
+            Student student3 = new Student() { ID = 3, Name = "Orlando Lopez", Program = program3 };
+            Student student4 = new Student() { ID = 4, Name = "Cristiano Ronaldo", Program = program4 };
 
         }
+
+       /* public School GetStudents()
+        {
+            foreach (var student in Students)
+            {
+                return student.ToString();
+            }
+
+        } */
 
     }
 }
