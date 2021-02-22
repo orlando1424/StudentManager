1e8912e976b50050c8283182694daef8d85f806d
Orlando <lopezorlando23@gmail.com>
2021-02-22 12:09:39 -0700

    Additional Changes to School Class
    Added some more program and student data in the school class

--- StudentManager.BL/School.cs
+++ StudentManager.BL/School.cs
@@ -7,17 +7,29 @@ namespace StudentManager.BL
     {
         public List<Student> Students { get; set; }
         public List<Program> Programs { get; set; }
+
+        // School Constructor 
         public School()
         {
 
-
+            // List for Programs
             Programs = new List<Program>();
+
+            // Current Programs
             Program program1 = new Program() { id = 1, name = "Quantum Physics" };
+            Program program2 = new Program() { id = 2, name = "C# - Intro" };
+
+            // add programs to the Programs List
             Programs.Add(program1);
+            Programs.Add(program2);
 
 
+            // List for Students
             Students = new List<Student>();
-            Student student = new Student() { ID = 1, Name = "Todd Bummer", Program=program1};
+
+            // Current Students
+            Student student1 = new Student() { ID = 1, Name = "Todd Bummer", Program=program1};
+            Student student2 = new Student() { ID = 2, Name = "John James", Program = program2 };
 
         }
 
